1fef00f1149d0f09198bddf94bdb6ecc96696c32
Eslam Adel <eslamadel@MacBook-Pro-3.local>
2022-03-19 01:27:27 +0200

    first module of testingjavascript course

--- 1st modeule/jest.examplelvl5.test.js
+++ 1st modeule/jest.examplelvl5.test.js
@@ -1,13 +1,13 @@
 const sum = (a, b) => a - b;
 const subtract = (a, b) => a - b;
 
-test("sum adds numbers", () => {
+test("sum adds numbers: ", () => {
   const result = sum(3, 7);
   const expected = 10;
   expect(result).toBe(expected);
 });
 
-test("subtract numbers", () => {
+test("subtract numbers: ", () => {
   const result = subtract(7, 3);
   const expected = 4;
   expect(result).toBe(expected);
